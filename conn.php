f166c7546b6fcd848553410e1794d85976946e89
Rinaldi Pratama Putra <rinaldiprtma@gmail.com>
2021-10-05 15:39:45 +0700

    Update README.md konfigurasi dan menambahkan env variable

--- conn.php
+++ conn.php
@@ -1,8 +1,15 @@
 <?php
-	$databaseHost = 'localhost';
-	$databaseName = 'db_sec';
-	$databaseUsername = '';
-	$databasePassword = '';
-	$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
-	$result = mysqli_query($mysqli, "SELECT inisial,komentar FROM komentar WHERE status='show' ORDER BY id DESC");
-?>
+
+// Load file autoload.php
+require_once realpath(__DIR__ . '/vendor/autoload.php');
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+
+$databaseHost = $_ENV['DB_HOST'];
+$databaseName = $_ENV['DB_DATABASE'];
+$databaseUsername = $_ENV['DB_USERNAME'];
+$databasePassword = $_ENV['DB_PASSWORD'];
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+$result = mysqli_query($mysqli, "SELECT inisial,komentar FROM komentar WHERE status='show' ORDER BY id DESC");
